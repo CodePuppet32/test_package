97a5531bab6ad022e867065bb688c0087bb2cfa0
CodePuppet32 <63444107+CodePuppet32@users.noreply.github.com>
2022-08-08 12:32:18 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='tests
+    version='0.0.3',
+    author='Harsh',
+    author_email='harsh.harsh@xperi.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mike-huls/toolbox',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    },
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['requests'],
+)
